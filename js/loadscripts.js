0c1249bc6f150e0fc353ac9bb4542ae8ca945a3a
wt29 <tglynn@hotmail.com>
2023-06-10 18:14:39 +1000

    Fixed scripts

--- js/loadscripts.js
+++ js/loadscripts.js
@@ -7,18 +7,6 @@ var scripts = /*html*/ `
 <script src="js/jquery.responsiveTabs.js"></script>
 <script src="js/videobox.js"></script>
 <script src="js/featherlight.js"></script>
-<script src="js/createform.js"></script>
 <script src="js/persist.js"></script>
-<script defer src="js/encoding.js"></script>
-<script defer src="js/encoding-indexes.js"></script>
-<script defer src="js/gcodeprocessing.js"></script>
-<script defer src="js/commongcode.js"></script>
-<script defer src="js/firstlayer.js"></script>
-<script defer src="js/baseline.js"></script>
-<script defer src="js/retraction.js"></script>
-<script defer src="js/temperature.js"></script>
-<script defer src="js/acceleration.js"></script>
-<script defer src="js/speed.js"></script>
-<script defer src="js/minimumlength.js"></script>
 `;
 document.write(scripts);
